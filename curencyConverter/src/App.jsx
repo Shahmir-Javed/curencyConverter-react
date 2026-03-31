49112969dee2515f31f1ba74dc6a1eb3afa13588
Shahmir Javed <malikshahmir098@gmail.com>
2026-03-31 14:18:36 +0500

    project Setup

---
+++ curencyConverter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<h1 className='text-cyan-100 text-3xl border-2 bg-orange-400'>Hello Shahmir</h1>
+  )
+}
+
+export default App
